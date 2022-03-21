afe84e5ad2e9b33841195cbecd801bac7c38b90f
BluesyDank <70462949+thescienceofnerds@users.noreply.github.com>
2022-03-21 20:09:11 +0000

    Update keystrokes for keylogger function (console).py

--- keystrokes for keylogger function (console).py
+++ keystrokes for keylogger function (console).py
@@ -4,7 +4,7 @@
 from pynput.keyboard import Key, Listener # pip installpynput, pip install Listener
 
 def on_press(key):       # defines that when a key is pressed...
-    print(key)    # ... It prints the name of that key in the console
+    print(key)    # ... then prints the name of that key in the console
 
 with Listener(on_press=on_press) as listener: # uses listener to record a keypress, whenever a key is pressed
     listener.join() # tells the listener to listen
